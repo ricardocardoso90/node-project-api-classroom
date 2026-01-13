8fc875d364495d7a818decf22ca8cf7d53494a81
Ricardo Cardoso <ricardocardoso90@gmail.com>
2026-01-13 15:01:57 -0300

    update commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
 import http from "http";
 
-console.log("Server is running on port 3000");
+
+
+const server = http.createServer((req, res) => {
+  return res.end("Hello World!");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
