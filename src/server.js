bafaf444ef337c6a4640754997ca219ef9604b25
Ricardo Cardoso <ricardocardoso90@gmail.com>
2026-01-13 17:44:51 -0300

    update commit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 import http from "http";
+import express from "express";
 
+const app = express();
 
-
-const server = http.createServer((req, res) => {
-  return res.end("Método HTTP: " + req.method);
+app.get("/", (req, res) => {
+  return res.send("Método HTTP: " + req.method);
 });
 
-server.listen(3000, () => {
+// const server = http.createServer((req, res) => {
+//   return res.end("Método HTTP: " + req.method);
+// });
+
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+// server.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
