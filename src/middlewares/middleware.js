0b96af1d8fa192d593816dbc9f9a3068b656152a
Ricardo Cardoso <ricardocardoso90@gmail.com>
2026-01-13 21:55:58 -0300

    update commit

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,5 +1,7 @@
-export async function middleware(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+export async function middleware(req, res, next) {
+  if (req.method === "GET" && req.path === "/products/1") {
+    return res.status(405).json({ error: "Método não permitido" });
   };
+
+  next();
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from "express";
+import { middleware } from "./middlewares/middleware.js";
 
 const app = express();
 app.use(express.json());
 
-app.post("/products", (req, res) => {
+app.post("/products", middleware, (req, res) => {
   const { name, idade } = req.body;
 
-  console.log({name, idade});
+  console.log({ name, idade });
   return res.json({ name, idade });
 });
 
